59621b530b8f1d8f0939d912c0180ff426b0f795
Yishai A Statter <ystatter@chencloud1.(none)>
2015-05-14 09:05:11 -0600

    missing files committed

---
+++ Etc/utils/templates.tcl
@@ -0,0 +1,73 @@
+proc spice_object {name body} {
+    puts "Info: Defining Spice object $name"
+    print_line "typedef struct \{"
+        print_line $body
+    print_line "\} $name;"
+    print_line "void serial_$name\($name *i_$name\);"
+    print_line "void serial_read_$name\($name *i_$name\);"
+}
+
+
+proc For: {initial condition step body} {
+    uplevel $initial
+while {[uplevel "expr $condition"]} {
+        uplevel [list tcl_preprocessor $body]
+        uplevel $step
+    }
+}
+proc If: {condition body {else {}}} {
+if {[uplevel "expr $condition"]} {
+        uplevel [list tcl_preprocessor $body]
+    } else {
+        uplevel [list tcl_preprocessor $else]
+    }
+}
+
+proc Close_Bracket: {{num 1}} {
+print_line [string repeat "\}" $num]
+}
+proc Open_Bracket: {{num 1}} {
+print_line [string repeat "\{" $num]
+}
+proc Foreach: {iterator_var list body} {
+    upvar $iterator_var iterator
+    foreach iterator $list {
+        uplevel [list tcl_preprocessor $body]
+    }
+}
+proc define_message_templates {} {
+    switch $::target_flag {
+        TCL_MSG_REG {
+        set printed {Info Warning Error Print Nl Token}
+        set silent {Dinfo Dwarning Derror Dprint Dnl Dtoken}
+        }
+        TCL_MSG_SILENT {
+        set printed {}
+        set silent {Info Warning Error Print Nl Token Dinfo Dwarning Derror Dprint Dnl Dtoken}
+        } 
+        TCL_MSG_DEBUG {
+        set printed {Info Warning Error Print Nl Token Dinfo Dwarning Derror Dprint Dnl Dtoken}
+        set silent {}
+        }
+    }
+    foreach cmd $printed {
+        regsub -all @ {
+        proc @: {pattern args} {
+                set line "sprintf(tcl_print_buffer,\"@: $pattern\\n\""
+                foreach field $args {
+                    append line ,
+                    append line $field
+                }
+                append line ");"
+                print_line $line
+                print_line "if (Tcl_Eval(interp,tcl_print_buffer)==TCL_ERROR) Tcl_Eval(interp,\"puts \$::errorInfo\");"
+            }
+        } $cmd body
+        eval $body
+    }
+    foreach cmd $silent {
+    proc $cmd: args {}
+    }
+}
+
+
--- link.tcsh
+++
@@ -1,3 +0,0 @@
-#!/bin/tcsh
-setenv PATH /opt/centos/devtoolset-1.0/root/usr/bin/:$PATH
-g++ -L /usr/bin/lib -lm -ltcl8.5  -ldl  -ldb-4.7  /tmp/ramspice_build/object_files-silent/*.o -o /home/ystatter/elgon/bin/ramspice-silent | & tee -a log
