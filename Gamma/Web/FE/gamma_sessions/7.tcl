3d4849ee07803076a2da61907ca5ffec3044c6f9
Yishai <ystatter@linuxa1.engr.colostate.edu>
2015-08-28 14:16:38 -0600

    Avoiding PAT in GIT

---
+++ Gamma/Web/FE/gamma_sessions/7.tcl
@@ -0,0 +1,27 @@
+array set ::SESSION {
+Adc {}
+Area {}
+BW {}
+CMRR {}
+Nt {}
+PSRR {}
+Power {}
+Rout {}
+Vos {}
+ajax_cmd generate_map
+circuit_list {6493 9195 9795}
+deselect {}
+fc {}
+focus_circuit 55921
+ip {}
+selcircuit_10933 0
+selcircuit_14033 0
+selcircuit_14704 0
+selected_axes Adc%2CRout%2Cnone
+selected_circuits_tags {14033 10933 14704}
+selected_g {}
+selected_tech tsmc040
+selected_topology op_amp
+spice_netlist /net/nasstore/students/GRAD/ECE/ystatter/home/public_html/cgi-bin/ramspice/../../7.sn
+ts {}
+}
